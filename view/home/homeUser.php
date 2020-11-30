2fac7043617e13af9fa6977e707b18c47420d4bd
giuseppefiscarelli <giuseppe.fiscarelli@setec.it>
2020-11-30 12:41:55 +0100

    preprod

--- view/home/homeUser.php
+++ view/home/homeUser.php
@@ -1,3 +1,36 @@
-<h3>Salve <?=$_SESSION['userData']['nome']?> <?=$_SESSION['userData']['cognome']?></h3>
+<h3>Benvenuto <?=$_SESSION['userData']['ragione_sociale']?></h3>
 
-<h4>benvenuto nell'area..............</h4>
+
+<h4>Per visualizzare Le Istanze, Clicca su "Le Mie Istanze"</h4>
+
+<div class="it-carousel-wrapper it-carousel-landscape-abstract">
+          <div class="it-carousel-all owl-carousel" >
+            <div class="it-single-slide-wrapper" >
+              <a href="#">
+                <div class="img-responsive-wrapper">
+                  <div class="img-responsive">
+                    <div class="img-wrapper"><img src="images/img1.jpg" title="img title" alt="ima1"></div>
+                  </div>
+                </div>
+              </a>
+              <div class="it-text-slider-wrapper-outside">
+                <div class="card-wrapper">
+                  <div class="card">
+                    <div class="card-body">
+                      <div class="category-top" style="display:none;">
+                        <!-- category heading--><a class="category" href="#">Category</a>
+                        <!-- category data--><span class="data">10/12/2018</span>
+                      </div>
+                      <h5 class="card-title big-heading">Decreto del Ministero delle infrastrutture e dei trasporti n. 203 del 12 maggio 2020</h5>
+                      <h4 class="card-title big-heading">       Incentivo agli investimenti nel settore dell’autotrasporto</h4>
+                      <p style="text-align:justify;"class="card-text">Il D.M. 12 maggio 2020 n. 203, recante modalità di erogazione dei contributi a favore degli investimenti delle imprese di autotrasporto, per l'annualità 2020, è stato pubblicato nella Gazzetta Ufficiale Serie Generale n. 187 del 27 luglio 2020. Il connesso Decreto direttoriale prot. 145 del 07 agosto 2020 recante disposizioni di attuazione del suddetto D.M. 203/2020 è stato pubblicato nella Gazzetta Ufficiale Serie Generale n. 206 del 19 agosto 2020.</p>
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            
+          </div>
+        </div>
