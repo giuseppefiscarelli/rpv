a9c1da9525c2b5c4f26d63a7f87864e51c730f0c
giuseppefiscarelli <giuseppe.fiscarelli@setec.it>
2020-11-30 11:23:03 +0100

    preprod

--- view/home/homeAdmin.php
+++ view/home/homeAdmin.php
@@ -1,6 +1,75 @@
-<h1>Hello</h1>
- <p><?=$_SESSION['userData']['nome']?></p>
- <?php
-   // var_dump(phpinfo());
- ?>
-     
+<div class="row">
+        <div class="col-12 ">
+          <div class="card-wrapper card-space">
+            <div class="card card-bg">
+            
+              <div class="card-body">
+              <h3 class="card-title" style="align-text:center;">Istanze "nome incentivo"</h3>
+
+              <div class="row">
+                <div class="col-lg-3 col-12">
+                  <div class="card card-teaser rounded shadow" style="background-color: #96ceff">
+                  <div class="icon">
+                    <i class="fa fa-list" aria-hidden="true"></i>
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">
+                        Totali
+                      </h5>
+                      <div class="card-text">
+                      <b style="float:right;font-size: xx-large;"><?=$totalIstanze?></b>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-12">
+                  <div class="card card-teaser rounded shadow" style="background-color: #ff9c9c">
+                  <div class="icon">
+                    <i class="fa fa-list" aria-hidden="true"></i>
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">
+                      Attive
+                      </h5>
+                      <div class="card-text">
+                      <b style="float:right;font-size: xx-large;"><?=$totalIstanze?></b>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-12">
+                  <div class="card card-teaser rounded shadow" style="background-color: #ffe196">
+                    <div class="icon">
+                    <i class="fa fa-list" aria-hidden="true"></i>
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">
+                    In Rendicontazione
+                      </h5>
+                      <div class="card-text">
+                      <b style="float:right;font-size: xx-large;"><?=$totalIstanze?></b>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-12">
+                  <div class="card card-teaser rounded shadow" style="background-color: #96ffa1">
+                    <div class="icon">
+                    <i class="fa fa-list" aria-hidden="true"></i>
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">
+                      In Istruttoria
+                      </h5>
+                      <div class="card-text">
+                      <b style="float:right;font-size: xx-large;"><?=$totalIstanze?></b>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
