17d6ed3944152c4edcb5ca145ebfb8ec19b68d3a
giuseppefiscarelli <giuseppe.fiscarelli@setec.it>
2021-07-02 11:05:22 +0200

    infomodal

--- view/istanze/infomodal.php
+++ view/istanze/infomodal.php
@@ -155,14 +155,7 @@
                                 <th scope="row">Banca</th>
                                 <td id="info_banca"></td>
                                 </tr>
-                                <tr>
-                                <th scope="row">Maggiorazione PMI</th>
-                                <td id="info_pmi"></td>
-                                </tr>
-                                <tr>
-                                <th scope="row">Maggiorazione Contratto rete d'imprese</th>
-                                <td id="info_rete"></td>
-                                </tr>
+                               
                                 
                             </tbody>
                         </table>
@@ -182,7 +175,7 @@
               <thead >
                 <tr >
                   <th colspan="5" style="text-align:center;">
-                  Acquisizione dei veicoli a trazione alternativa a metano CNG e gas naturale liquefatto LNG, nonché a trazione elettrica - art. 1, comma 5, lett. a  
+                  Acquisizione di veicoli nuovi di fabbrica di massa complessiva a pieno carico pari o superiore a 3,5 tonnellate a trazione alternativa a metano CNG, gas naturale liquefatto LNG, ibrida(diesel/elettrico) ed elettrica (full-electric) - (art. 2, comma 1, lett. a. DI 335/2020)   
                   </th>      
                 </tr>
                 <tr >
@@ -194,98 +187,48 @@
                   </th>
                   <th style="text-align:center;">spesa prevista<br> (IVA esclusa)
                   </th>
-                  <th style="text-align:center;">eventuale<br> rottamazione
-                  </th>      
+                       
                 </tr>
               </thead>
               <tbody>
               <tr>
-                <td rowspan="3" style="vertical-align:middle;text-align:center;">CNG</td>
-                <td>DA 3,5 A 7 T.</td>
+                <td rowspan="2" style="vertical-align:middle;text-align:center;">CNG e ibrida</td>
+                <td>da 3,5 e inferiore a 7 t.</td>
                 <td id="info_nv1" style="text-align:right;"></td>
                 <td id="info_sp1" style="text-align:right;"></td>
-                <td id="info_rott1" style="text-align:right;"></td>                  
+                              
               </tr>
 
               <tr>
                
-                <td>OLTRE 7 E MENO DI 16 T.</td>
+                <td>da 7 e inferiore a 16 t.</td>
                 <td id="info_nv2" style="text-align:right;"></td>
                 <td id="info_sp2" style="text-align:right;"></td>
-                <td id="info_rott2" style="text-align:right;"></td>
+                
               </tr>
 
+             
+
               <tr>
-               
-                <td>DA 16 T.</td>
+                <td style="vertical-align:middle;text-align:center;">Elettrica</td>
+                <td>da 3,5 ed inferiore a 16 t.</td>
                 <td id="info_nv3" style="text-align:right;"></td>
                 <td id="info_sp3" style="text-align:right;"></td>
-                <td id="info_rott3" style="text-align:right;"></td>
+              
               </tr>
 
+
               <tr>
-                <td rowspan="2" style="vertical-align:middle;text-align:center;">LNG</td>
-                <td>DA 7 E MENO DI 16 T.</td>
+                <td  style="vertical-align:middle;text-align:center;">CNG, LNG,<br>ibrida ed elettrica</td>
+                <td>pari o superiore a 16 t.</td>
                 <td id="info_nv4" style="text-align:right;"></td>
                 <td id="info_sp4" style="text-align:right;"></td>
-                <td id="info_rott4" style="text-align:right;"></td>
+              
               </tr>
 
-              <tr>
-                
-                <td>DA 16 T.</td>
-                <td id="info_nv5" style="text-align:right;"></td>
-                <td id="info_sp5" style="text-align:right;"></td>
-                <td id="info_rott5" style="text-align:right;"></td>
-              </tr>
 
-              <tr>
-                <td rowspan="3" style="vertical-align:middle;text-align:center;">IBRIBA<br>(diesel/elettrico)</td>
-                <td>DA 3,5 A 7 T.</td>
-                <td id="info_nv6" style="text-align:right;"></td>
-                <td id="info_sp6" style="text-align:right;"></td>
-                <td id="info_rott6" style="text-align:right;"></td>
-              </tr>
 
-              <tr>
-                
-                <td>OLTRE 7 E MENO DI 16 T.</td>
-                <td id="info_nv7" style="text-align:right;"></td>
-                <td id="info_sp7" style="text-align:right;"></td>
-                <td id="info_rott7" style="text-align:right;"></td>
-              </tr>
 
-              <tr>
-                
-                <td>DA 16 T.</td>
-                <td id="info_nv8" style="text-align:right;"></td>
-                <td id="info_sp8" style="text-align:right;"></td>
-                <td id="info_rott8" style="text-align:right;"></td>
-              </tr>
-
-              <tr>
-                <td rowspan="2" style="vertical-align:middle;text-align:center;">ELETTRICA</td>
-                <td>DA 3,5 A 7 T.</td>
-                <td id="info_nv9" style="text-align:right;"></td>
-                <td id="info_sp9" style="text-align:right;"></td>
-                <td id="info_rott9" style="text-align:right;"></td>
-              </tr>
-
-              <tr>
-               
-                <td>OLTRE 7 T.</td>
-                <td id="info_nv10" style="text-align:right;"></td>
-                <td id="info_sp10" style="text-align:right;"></td>
-                <td id="info_rott10" style="text-align:right;"></td>
-              </tr>
-
-              <tr>
-                <td style="vertical-align:middle;text-align:center;">Dispositivi per la<br>riconversione a<br>trazione elettrica</td>
-                <td>3,5 T.</td>
-                <td id="info_nv11" style="text-align:right;"></td>
-                <td id="info_sp11" style="text-align:right;"></td>
-                <td ></td>
-              </tr>
 
               </tbody>          
             </table>
@@ -295,106 +238,40 @@
           <table class="table table-sm table-bordered" style="font-size:smaller;">
               <thead>
                 <tr>
-                  <th colspan="4" style="text-align:center;">Radiazione per rottamazione di veicoli pesanti di massa complessiva a pieno carico pari o superiore a 11,5 tonnellate, con contestuale acquisizione di veicoli nuovi di fabbrica art. 1, comma 5, lett. b</th>
+                  <th colspan="4" style="text-align:center;">Acquisizione di veicoli nuovi di fabbrica conformi alla normativa euro VI di massa complessiva a pieno carico pari o superiore a 3,5 tonnellate ai sensi di quanto previsto dall’art 10, commi 2 e 3, del Regolamento (CE) n. 595/2009 del Parlamento Europeo e del Consiglio del 18 giugno 2009 - (art. 2, comma 1, lett. b. DI 335/2020)</th>
                 </tr>
                 <tr>
-                  <th style="vertical-align:middle;text-align:center;">Tipo veicolo</th>
+                  <th style="vertical-align:middle;text-align:center;">Massa complessiva/tipo veicolo</th>
                   <th style="vertical-align:middle;text-align:center;">numero veicoli<br>per tipologia</th>
                   <th style="vertical-align:middle;text-align:center;">spesa prevista<br>(IVA esclusa)</th>
-                  <th style="vertical-align:middle;text-align:center;">Numero veicoli<br>rottamati</th>
+                  
                 </tr>
               </thead>
               <tbody>
               <tr>
-                <td  style="vertical-align:middle;text-align:center;">VEICOLI EURO 6 DI MASSA<br>COMPRESA FRA 7 E 16 T.</td>
-                <td id="info_r_nv_1" style="text-align:right;"></td>
-                <td id="info_r_sp_1" style="text-align:right;"></td>
-                <td id="info_r_rott_1" style="text-align:right;"></td>
+                <td  style="vertical-align:middle;text-align:center;">da 3,5 e inferiore a 7 t.<br>(euro VI / euro VI D Temp)</td>
+               
+                <td id="info_nv5" style="text-align:right;"></td>
+                <td id="info_sp5" style="text-align:right;"></td>
+              
               </tr>
               <tr>
-                <td  style="vertical-align:middle;text-align:center;">VEICOLI EURO 6 DI MASSA<br>SUPERIORE A 16 T.</td>
-                <td id="info_r_nv_2" style="text-align:right;"></td>
-                <td id="info_r_sp_2" style="text-align:right;"></td>
-                <td id="info_r_rott_2" style="text-align:right;"></td>
+                <td  style="vertical-align:middle;text-align:center;">da 7 e inferiore a 16 t.<br>(euro VI)</td>
+                
+                <td id="info_nv6" style="text-align:right;"></td>
+                <td id="info_sp6" style="text-align:right;"></td>
+              
               </tr>
               <tr>
-                <td  style="vertical-align:middle;text-align:center;">VEICOLI EURO 6 DTEMP DI MASSA<br>DA 3,5 E INFERIORE A 7 T.</td>
-                <td id="info_r_nv_3" style="text-align:right;"></td>
-                <td id="info_r_sp_3" style="text-align:right;"></td>
-                <td id="info_r_rott_3" style="text-align:right;"></td>
+                <td  style="vertical-align:middle;text-align:center;">pari o superiore a 16 t.<br>(euro VI)</td>
+               
+                <td id="info_nv7" style="text-align:right;"></td>
+                <td id="info_sp7" style="text-align:right;"></td>
+              
               </tr>
-
               </tbody>
           </table>
 
-          <table class="table table-sm table-bordered" style="font-size:smaller;">
-              <thead>
-                <tr>
-                  <th colspan="4" style="text-align:center;">ACQUISIZIONE ANCHE MEDIANTE LOCAZIONE FINANZIARIA, DI RIMORCHI E SEMIRIMORCHI, NUOVI DI FABBRICA, PER IL TRASPORTO COMBINATO – ART. 1, COMMA 5, LETT. C</th>
-                </tr>
-               
-              </thead>
-              <tbody>
-              <tr>
-                <td  colspan="2" rowspan="2"style="vertical-align:middle;text-align:center;">Rimorchi o semirimorchi UIC e IMO ciascuno<br>dotato di almeno uno dei dispositivi innovativi di cui<br>all’allegato 1 del D.M. 203 - 12 maggio 2020</td>
-                <th style="vertical-align:middle;text-align:center;">Nr. rimorchi o<br>semirimorchi:</th>
-                <th style="vertical-align:middle;text-align:center;">Spesa prevista<br>(IVA esclusa)</th>
-              </tr>
-              <tr>
-                <td id="info_nr_1" style="text-align:right;"></td>
-                <td id="info_spr_1" style="text-align:right;"></td>
-
-
-              </tr>
-
-              </tbody>
-          </table> 
-
-          <table class="table table-sm table-bordered" style="font-size:smaller;">
-              <thead>
-                <tr>
-                  <th colspan="4" style="text-align:center;">RIMORCHI, SEMIRIMORCHI E EQUIPAGGIAMENTI PER AUTOVEICOLI SPECIFICI SUPERIORI A 7 TONNELLATE ALLESTITI PER TRASPORTI IN REGIME ATP E SOSTITUZIONE DELLE UNITÀ FRIGORIFERE/CALORIFERE ART. 1, COMMA 5, LETT. C</th>
-                </tr>
-               
-              </thead>
-              <tbody>
-              <tr>
-                <td  colspan="2" rowspan="2"style="vertical-align:middle;text-align:center;width:63%;">Rimorchi, semirimorchi e equipaggiamenti, sostituzione delle unità frigorifere/calorifere installate, per veicoli superiori a 7 t.</td>
-                <th style="vertical-align:middle;text-align:center;">Nr. rimorchi, semirimorchi<br> ed equipaggiamenti</th>
-                <th style="vertical-align:middle;text-align:center;">Spesa prevista<br>(IVA esclusa)</th>
-              </tr>
-              <tr>
-                <td id="info_nr_2" style="text-align:right;"></td>
-                <td id="info_spr_2" style="text-align:right;"></td>
-
-
-              </tr>
-
-              </tbody>
-          </table> 
-
-          <table class="table table-sm table-bordered" style="font-size:smaller;">
-              <thead>
-                <tr>
-                  <th colspan="4" style="text-align:center;">ACQUISIZIONE DI GRUPPI DI CASSE MOBILI E RIMORCHI O SEMIRIMORCHI PORTACASSE ART. 1, COMMA 5, LETT. D</th>
-                </tr>
-               
-              </thead>
-              <tbody>
-              <tr>
-                <td  colspan="2" rowspan="2"style="vertical-align:middle;text-align:center;width:63%;">Gruppo costituito da 8 (otto) casse mobili ed un rimorchio o semirimorchio portacasse</td>
-                <th style="vertical-align:middle;text-align:center;">Nr. gruppi</th>
-                <th style="vertical-align:middle;text-align:center;">Spesa prevista<br>(IVA esclusa)</th>
-              </tr>
-              <tr>
-                <td id="info_ng_1" style="text-align:right;"></td>
-                <td id="info_spg_1" style="text-align:right;"></td>
-
-
-              </tr>
-
-              </tbody>
-          </table> 
 
           </div>
         </div>
@@ -446,79 +323,33 @@ $.ajax({
                       $('#info_cciaa').html(data.cciaa);
                       $('#info_codice_ateco').html(data.codice_ateco);
                       $('#info_banca').html(data.banca);
-                      if(data.pmi=="Yes"){data.pmi="Presente"}else{data.pmi="Non Presente"}
-                      if(data.rete=="Yes"){data.rete="Presente"}else{data.rete="Non Presente"}
-                      $('#info_pmi').html(data.pmi);
-                      $('#info_rete').html(data.rete);
+                     
                       formatter.format(data.costo)
                       $('#info_nv1').html(data.nv1);
                       $('#info_sp1').html(formatter.format(data.sp1));
-                      if(data.rott1=="Off"){data.rott1=" Non Presente"}
-                      if(data.rott1=="Yes"){data.rott1=" Presente"}
-                      $('#info_rott1').html(data.rott1);
+                     
                       $('#info_nv2').html(data.nv2);
                       $('#info_sp2').html(formatter.format(data.sp2));
-                      if(data.rott2=="Off"){data.rott2=" Non Presente"}
-                      if(data.rott2=="Yes"){data.rott2=" Presente"}
-                      $('#info_rott2').html(data.rott2);
+                      
                       $('#info_nv3').html(data.nv3);
                       $('#info_sp3').html(formatter.format(data.sp3));
-                      if(data.rott3=="Off"){data.rott3=" Non Presente"}
-                      if(data.rott3=="Yes"){data.rott3=" Presente"}
-                      $('#info_rott3').html(data.rott3);
+                     
                       $('#info_nv4').html(data.nv4);
                       $('#info_sp4').html(formatter.format(data.sp4));
-                      if(data.rott4=="Off"){data.rott4=" Non Presente"}
-                      if(data.rott4=="Yes"){data.rott4=" Presente"}
-                      $('#info_rott4').html(data.rott4);
+                     
                       $('#info_nv5').html(data.nv5);
                       $('#info_sp5').html(formatter.format(data.sp5));
-                      if(data.rott5=="Off"){data.rott5=" Non Presente"}
-                      if(data.rott5=="Yes"){data.rott5=" Presente"}
-                      $('#info_rott5').html(data.rott5);
+                     
                       $('#info_nv6').html(data.nv6);
                       $('#info_sp6').html(formatter.format(data.sp6));
-                      if(data.rott6=="Off"){data.rott6=" Non Presente"}
-                      if(data.rott6=="Yes"){data.rott6=" Presente"}
-                      $('#info_rott6').html(data.rott6);
+                     
                       $('#info_nv7').html(data.nv7);
                       $('#info_sp7').html(formatter.format(data.sp7));
-                      if(data.rott7=="Off"){data.rott7=" Non Presente"}
-                      if(data.rott7=="Yes"){data.rott7=" Presente"}
-                      $('#info_rott7').html(data.rott7);
+                      
                       $('#info_nv8').html(data.nv8);
                       $('#info_sp8').html(formatter.format(data.sp8));
-                      if(data.rott8=="Off"){data.rott8=" Non Presente"}
-                      if(data.rott8=="Yes"){data.rott8=" Presente"}
-                      $('#info_rott8').html(data.rott8);
-                      $('#info_nv9').html(data.nv9);
-                      $('#info_sp9').html(formatter.format(data.sp9));
-                      if(data.rott9=="Off"){data.rott9=" Non Presente"}
-                      if(data.rott9=="Yes"){data.rott9=" Presente"}
-                      $('#info_rott9').html(data.rott9);
-                      $('#info_nv10').html(data.nv10);
-                      $('#info_sp10').html(formatter.format(data.sp10));
-                      if(data.rott10=="Off"){data.rott10=" Non Presente"}
-                      if(data.rott10=="Yes"){data.rott10=" Presente"}
-                      $('#info_rott10').html(data.rott10);
-                      $('#info_nv11').html(data.nv11);
-                      $('#info_sp11').html(formatter.format(data.sp11));
-
-                      $('#info_r_nv_1').html(data.r_nv_1);
-                      $('#info_r_sp_1').html(formatter.format(data.r_sp_1));
-                      $('#info_r_rott_1').html(data.r_rott_1);
-                      $('#info_r_nv_2').html(data.r_nv_2);
-                      $('#info_r_sp_2').html(formatter.format(data.r_sp_2));
-                      $('#info_r_rott_2').html(data.r_rott_2);
-                      $('#info_r_nv_3').html(data.r_nv_3);
-                      $('#info_r_sp_3').html(formatter.format(data.r_sp_3));
-                      $('#info_r_rott_3').html(data.r_rott_3);
-                      $('#info_nr_1').html(data.nr_1);
-                      $('#info_spr_1').html(formatter.format(data.spr_1));
-                      $('#info_nr_2').html(data.nr_2);
-                      $('#info_spr_2').html(formatter.format(data.spr_2));
-                      $('#info_ng_1').html(data.ng_1);
-                      $('#info_spg_1').html(formatter.format(data.spg_1));
+                     
+                     
 
 
                        
