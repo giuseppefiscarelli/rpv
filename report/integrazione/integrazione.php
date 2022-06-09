48007efe855d5c8c62dbc3d5e0c136bbc7d924ba
giuseppefiscarelli <57521642+giuseppefiscarelli@users.noreply.github.com>
2022-06-09 19:02:21 +0200

    Update integrazione.php

--- report/integrazione/integrazione.php
+++ report/integrazione/integrazione.php
@@ -14,11 +14,11 @@ require_once  '../../vendor/autoload.php';
 require_once  '../../model/istanze.php';
 require_once  '../../functions.php';
 
-$rep = getReportId($_GET['id']);
-$user = getIstanza($rep['id_RAM']);
-$dettagli = getDettReport($_GET['id']);
-$tipo = $_GET['tipo'];
-$tipo_istanza= getTipoIstanza($user['tipo_istanza']);
+//$rep = getReportId($_GET['id']);
+//$user = getIstanza($rep['id_RAM']);
+///$dettagli = getDettReport($_GET['id']);
+//$tipo = $_GET['tipo'];
+//$tipo_istanza= getTipoIstanza($user['tipo_istanza']);
 
 use Spipu\Html2Pdf\Html2Pdf;
 use Spipu\Html2Pdf\Exception\Html2PdfException;
@@ -28,19 +28,20 @@ try {
     $html2pdf = new Html2Pdf('P', 'A4', 'en', true, 'UTF-8', array(5, 5, 5, 2),true);
     $html2pdf->pdf->SetDisplayMode('fullpage');
     $html2pdf->pdf->SetProtection(array('print','copy'));
-    //$html2pdf->setDefaultFont('times', 'serif');
+    $html2pdf->setDefaultFont('times', 'serif');
     ob_start();
-    include dirname(__FILE__).'/res/test.php';
+    //include dirname(__FILE__).'/res/test.php';
     
      
     
-    $content = ob_get_clean();
-   // $path = $pathReport;
-    $html2pdf->writeHTML($content);
+    //$content = ob_get_clean();
+    //$path = $pathReport;
+    //$html2pdf->writeHTML($content);
     $filename = $rep['id']."_".$rep['id_RAM']."_".time();
     
     //$html2pdf->createIndex('Sommaire', 30, 12, false, true, 2, null, '10mm');
     if($tipo =="P"){
+        $html2pdf->writeHTML('<h1>HelloWorld</h1>This is my first test');
         $html2pdf->output($filename.".pdf",'I');
     }
     if($tipo =="D"){
